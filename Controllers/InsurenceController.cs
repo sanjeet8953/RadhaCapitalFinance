5bf89e15f57f151224854ccd1070271aa12917b0
sanjeet8953 <sanjeetkumar639292@gmail.com>
2025-08-24 19:36:49 +0530

    main page created done

---
+++ Controllers/InsurenceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadhaCapitalFinance.Controllers
+{
+    public class InsurenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Finence()
+        {
+            return View();
+        }
+    }
+}
--- Models/InsurenceModel.cs
+++ Models/InsurenceModel.cs
@@ -1,6 +1,23 @@
-﻿namespace RadhaCapitalFinance.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RadhaCapitalFinance.Models
 {
     public class InsurenceModel
     {
+        [Required(ErrorMessage = "Select a product")]
+        public string? Product { get; set; }
+
+        [Required(ErrorMessage = "Enter your name")]
+        public string? Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Enter your mobile number")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Enter valid 10 digit mobile number")]
+        public string? Mobile { get; set; }
+
+        [Required(ErrorMessage = "You must agree to Terms & Conditions")]
+        public bool Consent { get; set; }
     }
 }
