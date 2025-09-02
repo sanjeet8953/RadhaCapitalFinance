188170b1cbab97153b7676161b17d915fe89ce6b
sanjeet8953 <sanjeetkumar639292@gmail.com>
2025-09-02 15:48:45 +0530

    Recovered files from stash

---
+++ Models/PropertyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RadhaCapitalFinance.Models
+{
+    public class PropertyModel
+    {
+        public string? MobailNo { get; set; }
+        public string? ShopName { get; set; }
+        public int Pincode { get; set; }
+        public string? Email { get; set; }
+        public bool Consent { get; set; }
+    }
+}
